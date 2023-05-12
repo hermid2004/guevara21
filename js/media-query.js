439013686e4c1f097fc14994ff25103db2ba89fb
hermid2004 <hermidguevarapalechor4@gmail.com>
2023-05-12 11:49:35 -0500

    cambios

--- js/media-query.js
+++ js/media-query.js
@@ -1,10 +1,28 @@
 let menuBar = document. getElementById("menuBar")
+let mainNav = document. getElementById("mainNav")
+let mainMenu = document. getElementById("mainMenu")
 
 menuBar.addEventListener("click", function(){
- alert("Bienvenidosa a js")
- if (menuBar.classList.contains("menu-media-hidden")){
+   if(mainMenu.classList.contains("main-menu")){
+      mainMenu.classList.add("main-menu-block");
+      mainMenu.classList.remove("main-menu");
+   }
+   else {
+     mainMenu.classList.add("main-menu");
+     mainMenu.classList.remove("main-menu-block");
+   }
+});
+
+
+
+/*   if (menuBar.classList.contains("menu-media-hidden")){
     menuBar.classList.add("menu-media-hidden");
     menuBar.classList.remove("menu-media-hidden");
  }
-});
+ else {
+
+ } */
+   /* alert("Bienvenidosa a js") */
+/*    mainNav.classList.remove("main-nav");
+   mainNav.classList.add("main-nav-2"); */
 

2a066686dde74bf0ffe3dcce1bcee6a8490a47c8
hermid2004 <hermidguevarapalechor4@gmail.com>
2023-09-22 07:16:58 -0500

    cambios

---
+++ js/slider.js
@@ -0,0 +1,33 @@
+let iconChevronLeft = document.getElementById("iconChevronLeft");
+let iconChevronRight = document.getElementById("iconChevronRight");
+let boxCarousel = document.getElementById("boxCarousel");
+
+iconChevronRight.addEventListener("click", function(){
+    if(boxCarousel.classList.contains("box-carousel-1")) {
+        boxCarousel.classList.remove("box-carousel-1");
+        boxCarousel.classList.add("box-carousel-2");
+    }
+    else if (boxCarousel.classList.contains("box-carousel-2")) {
+        boxCarousel.classList.remove("box-carousel-2");
+        boxCarousel.classList.add("box-carousel-3");
+    }
+    else if (boxCarousel.classList.contains("box-carousel-3")) {
+        boxCarousel.classList.remove("box-carousel-3");
+        boxCarousel.classList.add("box-carousel-4");
+    }
+});
+
+iconChevronLeft.addEventListener("click", function(){
+    if(boxCarousel.classList.contains("box-carousel-2")) {
+        boxCarousel.classList.remove("box-carousel-2");
+        boxCarousel.classList.add("box-carousel-1");
+    }
+    else if (boxCarousel.classList.contains("box-carousel-3")) {
+        boxCarousel.classList.remove("box-carousel-3");
+        boxCarousel.classList.add("box-carousel-2");
+    }
+    else if (boxCarousel.classList.contains("box-carousel-4")) {
+        boxCarousel.classList.remove("box-carousel-4");
+        boxCarousel.classList.add("box-carousel-3");
+    }
+});
